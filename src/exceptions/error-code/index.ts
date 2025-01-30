1ed68797ea942fce0047d85db2440b491a7a9a5d
정회민 <wony1102@naver.com>
2025-01-30 17:23:13 +0900

    feat: Exception Global Filter 적용
    어떠한 에러가 발생해도 공통적인 에러 class를 응답하도록 설정

---
+++ src/exceptions/error-code/index.ts
@@ -0,0 +1,10 @@
+class ErrorCode {
+  readonly status: number;
+  readonly message: string;
+  constructor(status: number, message: string) {
+    this.status = status;
+    this.message = message;
+  }
+}
+
+export default ErrorCode;
