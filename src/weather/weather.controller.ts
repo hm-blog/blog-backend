cabf7f61d563e553f6cb6b2174aa9b7302451ad5
정회민 <wony1102@naver.com>
2025-01-30 17:37:51 +0900

    feat: 외부 API를 사용해 원하는 좌표의 일기예보 데이터를 제공하는 API 추가
    [기상청_단기예보 ((구)_동네예보) 조회서비스]
    https://www.data.go.kr/data/15084084/openapi.do

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -5,4 +5,8 @@ import { WeatherService } from './weather.service';
 export class WeatherController {
   constructor(private readonly weatherService: WeatherService) {}
 
+  @Get()
+  async getWeather(@Query('latitude') latitude: number, @Query('longitude') longitude: number) {
+    return await this.weatherService.searchWeather({ latitude, longitude });
+  }
 }
