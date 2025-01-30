84d66cd225fb433e1f74029e8f588dab1ca3a78f
정회민 <wony1102@naver.com>
2025-01-30 17:20:25 +0900

    fix: RESTful API 경로에 GlobalPreFix 'api'로 적용

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(1818);
 }
